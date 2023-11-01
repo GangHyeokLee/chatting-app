e1c2e5d36b8ad5f97e418e04b1a1dad96969acbb
GangHyeok Lee <rkdgur5381@gmail.com>
2023-11-01 16:30:29 +0900

    ChatRoomInfo, Message Form

--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import { Button, Col, Form, ProgressBar, Row } from "react-bootstrap"
 
 function MessageForm() {
   return (
-    <div>MessageForm</div>
+    <div>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
+
+      <ProgressBar variant='warning' label='60%' now={60} />
+
+      <Row>
+        <Col>
+          <button
+            className="message-form-button"
+            style={{ width: '100%' }}
+          >
+            SEND
+          </button>
+        </Col>
+        <Col>
+          <button
+            className="message-form-button"
+            style={{ width: '100%' }}
+          >
+            UPLOAD
+          </button>
+        </Col>
+      </Row>
+
+    </div>
   )
 }
 
--- src/components/ChatPage/MainPanel/MessageHeader.js
+++ src/components/ChatPage/MainPanel/MessageHeader.js
@@ -1,8 +1,80 @@
 import React from 'react'
+import { Accordion, Button, Card, Col, Container, Form, Image, InputGroup, Row, useAccordionButton } from "react-bootstrap"
+import { FaLock } from "react-icons/fa"
+import { MdFavorite } from 'react-icons/md'
+import { AiOutlineSearch } from 'react-icons/ai'
 
 function MessageHeader() {
+
+  const CustomToggle = ({ children, eventKey }) => {
+    const decoratedOnClick = useAccordionButton(eventKey, () => {});
+  
+    return (
+      <Button
+        type="button"
+        style={{ border: 'none', background: 'none'}}
+        onClick={decoratedOnClick}
+        variant = 'link'
+      >
+        {children}
+      </Button>
+    );
+  }
+
   return (
-    <div>MessageHeader</div>
+    <div style={{
+      width: '100%',
+      height: '170px',
+      border: '.2rem solid #ececec',
+      borderRadius: '4px',
+      padding: '1rem',
+      marginBottom: '1rem'
+    }}>
+      <Container>
+        <Row>
+          <Col><h2><FaLock /> ChatRoomName <MdFavorite /></h2></Col>
+          <Col><InputGroup className="mb-3">
+            <InputGroup.Text id="basic-addon1"><AiOutlineSearch /></InputGroup.Text>
+            <Form.Control
+              placeholder="Search Messages"
+              aria-label="Search"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup></Col>
+        </Row>
+        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <p>
+            <Image src="" />{" "} user name
+          </p>
+        </div>
+        <Row>
+          <Col>
+            <Accordion>
+              <Card>
+                <Card.Header>
+                  <CustomToggle  eventKey="0">Click me!</CustomToggle>
+                </Card.Header>
+                <Accordion.Collapse   eventKey="0">
+                  <Card.Body>Hello! I'm the body</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </Col>
+          <Col>
+          <Accordion>
+              <Card>
+                <Card.Header>
+                  <CustomToggle variant = 'link' eventKey="0">Click me!</CustomToggle>
+                </Card.Header>
+                <Accordion.Collapse   eventKey="0">
+                  <Card.Body>Hello! I'm the body</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
